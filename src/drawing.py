c8fc829f43be6dcf760a41fa1c6d8854b2768ede
Kirill Zavadski <kiriller754@gmail.com>
2024-12-12 23:28:57 +0300

    demon rage v1

---
+++ src/drawing.py
@@ -0,0 +1,197 @@
+from pkgutil import resolve_name
+
+import pygame
+from settings import *
+from ray_casting import ray_casting
+from collections import deque
+from random import randrange
+import sys
+
+
+class Drawing:
+    def __init__(self, sc, sc_map, player, clock):
+        self.sc = sc
+        self.sc_map = sc_map
+        self.player = player
+        self.clock = clock
+        self.font = pygame.font.SysFont('Arial', 36, bold=True)
+        self.font_win = pygame.font.Font('font/main_font.ttf', 144)
+        self.textures = {1: pygame.image.load('img/wall/wall1.png').convert(),
+                         2: pygame.image.load('img/wall/wall2.png').convert(),
+                         3: pygame.image.load('img/wall/wall3.png').convert(),
+                         4: pygame.image.load('img/wall/wall4.png').convert(),
+                         5: pygame.image.load('img/wall/wall5.png').convert(),
+                         6: pygame.image.load('img/wall/wall6.png').convert(),
+                         7: pygame.image.load('img/wall/wall7.png').convert(),
+                         'S': pygame.image.load('img/sky2.png').convert()
+                         }
+
+        # menu
+        self.menu_trigger = True
+        self.menu_picture = pygame.image.load('img/bg.jpg').convert()
+
+        # weapon parameters
+        self.weapon_base_sprite = pygame.image.load('sprites/weapons/shotgun/base/0.png').convert_alpha()
+        self.weapon_shot_animation = deque([pygame.image.load(f'sprites/weapons/shotgun/shot/{i}.png').convert_alpha()
+                                            for i in range(20)])
+        self.weapon_rect = self.weapon_base_sprite.get_rect()
+        self.weapon_pos = (HALF_WIDTH - self.weapon_rect.width // 2, HEIGHT - self.weapon_rect.height)
+        self.shot_projection = 1
+        self.shot_length = len(self.weapon_shot_animation)
+        self.shot_length_count = 0
+        self.shot_animation_speed = 3
+        self.shot_animation_count = 0
+        self.shot_animation_trigger = True
+        self.shot_sound = pygame.mixer.Sound('sound/shotgun.wav')
+
+        # sfx parameters
+        self.sfx = deque([pygame.image.load(f'sprites/weapons/sfx/{i}.png').convert_alpha() for i in range(9)])
+        self.sfx_length_count = 0
+        self.sfx_length = len(self.sfx)
+
+    def background(self, angle):
+        sky_offset = -10 * math.degrees(angle) % WIDTH
+        self.sc.blit(self.textures['S'], (sky_offset, 0))
+        self.sc.blit(self.textures['S'], (sky_offset - WIDTH, 0))
+        self.sc.blit(self.textures['S'], (sky_offset + WIDTH, 0))
+        pygame.draw.rect(self.sc, DARKGRAY, (0, HALF_HEIGHT, WIDTH, HALF_HEIGHT))
+
+    def world(self, world_objects):
+        for obj in sorted(world_objects, key=lambda n: n[0], reverse=True):
+            if obj[0]:
+                _, object, object_pos = obj
+                self.sc.blit(object, object_pos)
+
+    def fps(self, clock):
+        display_fps = str(int(clock.get_fps()))
+        render = self.font.render(display_fps, 0, DARKORANGE)
+        self.sc.blit(render, FPS_POS)
+
+    # def mini_map(self, player):
+    #     self.sc_map.fill(BLACK)
+    #     map_x, map_y = player.x // MAP_SCALE, player.y // MAP_SCALE
+    #     pygame.draw.line(self.sc_map, YELLOW, (map_x, map_y), (map_x + 12 * math.cos(player.angle),
+    #                                              map_y + 12 * math.sin(player.angle)), 2)
+    #     pygame.draw.circle(self.sc_map, RED, (int(map_x), int(map_y)), 5)
+    #     for x, y in mini_map:
+    #         pygame.draw.rect(self.sc_map, DARKBROWN, (x, y, MAP_TILE, MAP_TILE))
+    #     self.sc.blit(self.sc_map, MAP_POS)
+
+    def player_weapon(self, shots):
+        if self.player.shot:
+            if not self.shot_length_count:
+                self.shot_sound.play()
+            self.bullet_sfx()
+            self.shot_projection = min(shots)[1]
+            shot_sprite = self.weapon_shot_animation[0]
+            self.sc.blit(shot_sprite, self.weapon_pos)
+            self.shot_animation_count += 1
+            if self.shot_animation_count == self.shot_animation_speed:
+                self.weapon_shot_animation.rotate(-1)
+                self.shot_animation_count = 0
+                self.shot_length_count += 1
+                self.shot_animation_trigger = False
+            if self.shot_length_count == self.shot_length:
+                self.player.shot = False
+                self.shot_length_count = 0
+                self.sfx_length_count = 0
+                self.shot_animation_trigger = True
+        else:
+            self.sc.blit(self.weapon_base_sprite, self.weapon_pos)
+
+    def player_life(self, player):
+        life_font = pygame.font.Font('font/label.ttf', 36)
+        life = life_font.render(f'{player.life}', True, RED)
+        life_rect = life.get_rect()
+        life_rect.x = 10
+        life_rect.y = HEIGHT - life_rect.height - 10
+        self.sc.blit(life, life_rect)
+
+    def bullet_sfx(self):
+        if self.sfx_length_count < self.sfx_length:
+            sfx = pygame.transform.scale(self.sfx[0], (self.shot_projection, self.shot_projection))
+            sfx_rect = sfx.get_rect()
+            self.sc.blit(sfx, (HALF_WIDTH - sfx_rect.w // 2, HALF_HEIGHT - sfx_rect.h // 2))
+            self.sfx_length_count += 1
+            self.sfx.rotate(-1)
+
+    def win(self):
+        render = self.font_win.render('YOU WIN!!!', True, (randrange(40, 120), 0, 0))
+        text_rect = render.get_rect()
+        text_rect.center = (HALF_WIDTH, HALF_HEIGHT)
+        bord_rect = pygame.Rect(0, 0, text_rect.width + 50, text_rect.height + 20)
+        bord_rect.center = HALF_WIDTH, HALF_HEIGHT
+        pygame.draw.rect(self.sc, BLACK, bord_rect, border_radius=50)
+        self.sc.blit(render, text_rect)
+        pygame.display.flip()
+        self.clock.tick(15)
+
+    def lose(self):
+        render = self.font_win.render('YOU LOSE!!',
+                                      True,
+                                      (randrange(40, 120), randrange(40, 120), 0))
+        text_rect = render.get_rect()
+        text_rect.center = (HALF_WIDTH, HALF_HEIGHT)
+        bord_rect = pygame.Rect(0, 0, text_rect.width + 50, text_rect.height + 20)
+        bord_rect.center = HALF_WIDTH, HALF_HEIGHT
+        pygame.draw.rect(self.sc, BLACK, bord_rect, border_radius=50)
+        self.sc.blit(render, text_rect)
+        pygame.display.flip()
+
+
+    def menu(self):
+        x = 0
+        button_font = pygame.font.Font('font/main_font.ttf', 72)
+        label_font = pygame.font.Font('font/label.ttf', 70)
+
+        start = button_font.render('START', True, pygame.Color('lightgray'))
+        button_start = pygame.Rect(0, 0, WIDTH // 3, HEIGHT // 6)
+        button_start.center = HALF_WIDTH, HEIGHT // 3 + HEIGHT // 6
+        start_rect = start.get_rect()
+        start_rect.center = HALF_WIDTH, HEIGHT // 3 + HEIGHT // 6
+
+        exit_label = button_font.render('EXIT', True, pygame.Color('lightgray'))
+        button_exit = pygame.Rect(0, 0, WIDTH // 3, HEIGHT // 6)
+        button_exit.center = HALF_WIDTH, start_rect.centery + button_start.h + 20
+        exit_rect = exit_label.get_rect()
+        exit_rect.center = HALF_WIDTH, start_rect.centery + button_start.h + 20
+
+        while self.menu_trigger:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.sc.blit(self.menu_picture, (0, 0), (x % WIDTH, HALF_HEIGHT, WIDTH, HEIGHT))
+            x += 1
+
+            pygame.draw.rect(self.sc, BLACK, button_start, border_radius=25, width=10)
+            self.sc.blit(start, start_rect)
+
+            pygame.draw.rect(self.sc, BLACK, button_exit, border_radius=25, width=10)
+            self.sc.blit(exit_label, exit_rect)
+
+            color = randrange(20, 70)
+            label = label_font.render('DEMON RAGE', True, (color + 30, color, color))
+            rect_label = label.get_rect()
+            rect_label.center = HALF_WIDTH, HEIGHT // 6
+            self.sc.blit(label, rect_label)
+
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_click = pygame.mouse.get_pressed()
+            if button_start.collidepoint(mouse_pos):
+                pygame.draw.rect(self.sc, BLACK, button_start, border_radius=25)
+                start_red = button_font.render('START', True, pygame.Color('red'))
+                self.sc.blit(start_red, start_rect)
+                if mouse_click[0]:
+                    self.menu_trigger = False
+            elif button_exit.collidepoint(mouse_pos):
+                pygame.draw.rect(self.sc, BLACK, button_exit, border_radius=25)
+                exit_red = button_font.render('EXIT', True, pygame.Color('red'))
+                self.sc.blit(exit_red, exit_rect)
+                if mouse_click[0]:
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.flip()
+            self.clock.tick(20)
---
+++ src/player.py
@@ -0,0 +1,118 @@
+import math
+import pygame
+from settings import *
+from map import collision_walls
+
+
+class Player:
+    def __init__(self, sprites):
+        self.x, self.y = player_pos
+        self.life = 100
+        self.sprites = sprites
+        self.angle = player_angle
+        self.sensitivity = 0.004
+        self.speed = player_speed
+        self.side = 50  # Player's hitbox size
+        self.rect = pygame.Rect(*player_pos, self.side, self.side)
+        self.shot = False  # Weapon state
+        self.last_damage_time = 0
+        self.damage_cooldown = 2
+
+    @property
+    def pos(self):
+        return self.x, self.y
+
+    @property
+    def collision_list(self):
+        """Combine static walls and blocked sprite objects into one collision list."""
+        return collision_walls + [
+            pygame.Rect(*obj.pos, obj.side, obj.side)
+            for obj in self.sprites.list_of_objects
+            if obj.blocked
+        ]
+
+    def damage(self, hit):
+        """ """
+        if self.life > hit:
+            self.life -= hit
+        elif self.life <= hit:
+            self.life = 0
+
+        # if self.life == 0:
+
+
+    def detect_collision(self, dx, dy):
+        """Check and handle collisions for given movement deltas."""
+        next_rect = self.rect.move(dx, dy)
+        collisions = next_rect.collidelistall(self.collision_list)
+
+        if collisions:
+            delta_x, delta_y = 0, 0
+            for index in collisions:
+                hit_rect = self.collision_list[index]
+                if dx > 0:  # Moving right
+                    delta_x += next_rect.right - hit_rect.left
+                elif dx < 0:  # Moving left
+                    delta_x += hit_rect.right - next_rect.left
+                if dy > 0:  # Moving down
+                    delta_y += next_rect.bottom - hit_rect.top
+                elif dy < 0:  # Moving up
+                    delta_y += hit_rect.bottom - next_rect.top
+
+            # Resolve collision priority
+            if abs(delta_x - delta_y) < 10:
+                dx, dy = 0, 0
+            elif delta_x > delta_y:
+                dy = 0
+            else:
+                dx = 0
+
+        # Update position after collision resolution
+        self.x += dx
+        self.y += dy
+
+    def movement(self):
+        """Handle all player movement logic."""
+        self.process_inputs()
+        self.rect.center = self.x, self.y
+        self.angle %= DOUBLE_PI
+
+    def process_inputs(self):
+        """Process keyboard and mouse inputs."""
+        keys = pygame.key.get_pressed()
+        sin_a, cos_a = math.sin(self.angle), math.cos(self.angle)
+
+        # Handle keyboard inputs for movement
+        dx = dy = 0
+        if keys[pygame.K_w]:  # Move forward
+            dx += self.speed * cos_a
+            dy += self.speed * sin_a
+        if keys[pygame.K_s]:  # Move backward
+            dx -= self.speed * cos_a
+            dy -= self.speed * sin_a
+        if keys[pygame.K_a]:  # Strafe left
+            dx += self.speed * sin_a
+            dy -= self.speed * cos_a
+        if keys[pygame.K_d]:  # Strafe right
+            dx -= self.speed * sin_a
+            dy += self.speed * cos_a
+        self.detect_collision(dx, dy)
+
+        # Handle mouse input
+        self.handle_mouse()
+
+        # Handle additional events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                self.shot = True
+
+    def handle_mouse(self):
+        """Handle mouse movement for changing the player's angle."""
+        if pygame.mouse.get_focused():
+            mouse_x = pygame.mouse.get_pos()[0]
+            difference = mouse_x - HALF_WIDTH
+            pygame.mouse.set_pos((HALF_WIDTH, HALF_HEIGHT))
+            self.angle += difference * self.sensitivity
